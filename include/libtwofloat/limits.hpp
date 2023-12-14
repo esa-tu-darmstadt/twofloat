34ac92471af5870a5730436aec0e8b6e1ffde25a
timnoack <noack@esa.tu-darmstadt.de>
2023-12-14 16:45:49 +0100

    Fix documentation in limits.hpp

--- include/libtwofloat/limits.hpp
+++ include/libtwofloat/limits.hpp
@@ -5,13 +5,13 @@
 #include <type_traits>
 
 namespace std {
-/// \brief Specialization of std::numeric_limits for twofloat::twofloat.
+/// \brief Specialization of std::numeric_limits for twofloat::two<T>.
 template <typename T>
 class numeric_limits<::twofloat::two<T>> : public numeric_limits<T> {
   // Make sure that T is a floating point type
   static_assert(
       is_floating_point<T>::value,
-      "twofloat::two<T> can only be instantiated with a floating point "
+      "twofloat::two<T> can only be specialized with a floating point "
       "type.");
 
  public:
