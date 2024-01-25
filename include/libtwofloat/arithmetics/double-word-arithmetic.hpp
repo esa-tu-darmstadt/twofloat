fe761537ce91b69e0352063b9deac505cd41c742
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2024-01-25 13:45:28 -0500

    Improvements for sin_taylor().

--- include/libtwofloat/arithmetics/double-word-arithmetic.hpp
+++ include/libtwofloat/arithmetics/double-word-arithmetic.hpp
@@ -535,10 +535,8 @@ static two<T> sin_taylor(const two<T> &input) {
   do {
     r = mul<doubleword::Mode::Accurate, true>(r, x);
     t = mul<doubleword::Mode::Accurate, true>(r, two<T>{(local_ptr_inv_fact+i)[0], (local_ptr_inv_fact+i)[1]});
-  /*
-    s = add(s, t);
+    s = add<doubleword::Mode::Accurate>(s, t);
     i += 2;
-  */
   } while (i < n_inv_fact && std::abs(to_double(t)) > thresh);
 
   return s;
